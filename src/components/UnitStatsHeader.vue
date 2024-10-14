915a4e3ff9c8ae58788cc702189b2e4aab785a6a
GvardianDLVII <przemeksz557@gmail.com>
2024-10-14 22:40:08 +0200

    add tooltips for weapons

--- src/components/UnitStatsHeader.vue
+++ src/components/UnitStatsHeader.vue
@@ -1,12 +1,20 @@
 <template>
-    <div class="d-flex">
-        <shield-value-display :value="template.powerShield" />
-        {{ $t(`objectNames.chassis.${template.chassis}`) }}
-        <span v-for="i in 8">
-            <span class="ml-3" v-if="template.weapons[i-1] && i % 2 == 1"> {{ $t(`objectNames.weapons.short.${template.weapons[i-1]}`) }} </span>
-            <span class="ml-1" v-if="template.weapons[i-1] && i % 2 == 0"> + {{ $t(`objectNames.weapons.short.${template.weapons[i-1]}`) }} </span>
-        </span>
-    </div>
+  <div class="d-flex">
+    <shield-value-display :value="template.powerShield" />
+    {{ $t(`objectNames.chassis.${template.chassis}`) }}
+    <span v-for="i in 8">
+      <v-tooltip v-if="template.weapons[i-1] && i % 2 == 1" :text="$t(`objectNames.weapons.full.${template.weapons[i-1]}`)" location="bottom">
+        <template v-slot:activator="{ props }">
+          <span v-bind="props" class="ml-3"> {{ $t(`objectNames.weapons.short.${template.weapons[i-1]}`) }} </span>
+        </template>
+      </v-tooltip>
+      <v-tooltip v-if="template.weapons[i-1] && i % 2 == 0" :text="$t(`objectNames.weapons.full.${template.weapons[i-1]}`)" location="bottom">
+        <template v-slot:activator="{ props }">
+          <span v-bind="props" class="ml-1"> + {{ $t(`objectNames.weapons.short.${template.weapons[i-1]}`) }} </span>
+        </template>
+      </v-tooltip>
+    </span>
+  </div>
 </template>
 
 <script lang="ts">
@@ -15,10 +23,10 @@ import { UnitTemplate } from './api';
 import ShieldValueDisplay from './ShieldValueDisplay.vue';
 
 export default {
-    components: {ShieldValueDisplay},
-    props: {
-        template: {type: Object as PropType<UnitTemplate>, required: true },
-    },
-    data: () => ({}),
+  components: { ShieldValueDisplay },
+  props: {
+    template: { type: Object as PropType<UnitTemplate>, required: true },
+  },
+  data: () => ({}),
 }
 </script>
